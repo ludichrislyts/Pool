e408c3b506421e14fa6b133881909c969ef9755c
JackMackerel <ludichrislyts@gmail.com>
2016-03-01 19:55:40 -0800

    add package files

---
+++ WebAPI/Controllers/UI/LoginController.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Web.Mvc
+
+Namespace WebAPI
+    Public Class LoginController
+        Inherits System.Web.UI.Page
+        Dim login = New ViewPage()
+
+        ' GET: Login
+        Function Index() As ActionResult
+            Return login
+        End Function
+    End Class
+End Namespace
